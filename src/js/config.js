c0e18aa336f00f8e5df5d844686300e29c33b8a4
curator69 <rushikeshkhillari10@gmail.com>
2021-12-20 22:29:32 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd211e4ef-4591-4494-b84e-83976d193468';
+export const MODAL_CLOSE_SEC = 2.5;
