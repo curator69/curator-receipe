386eee5eff4a511ee560bcf2c903a2c5fec35305
curator69 <rushikeshkhillari10@gmail.com>
2021-12-20 23:40:16 +0530

    Added readme

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'd211e4ef-4591-4494-b84e-83976d193468';
+export const KEY = 'e68cd8c5-7a85-4767-ba26-aacc6e97ea36';
 export const MODAL_CLOSE_SEC = 2.5;
