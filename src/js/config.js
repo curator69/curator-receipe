ca53143e775c144cdb1ee54624fc1af6c24b3f9d
curator69 <rushikeshkhillari10@gmail.com>
2022-02-23 18:06:01 +0530

    update

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'e68cd8c5-7a85-4767-ba26-aacc6e97ea36';
+export const KEY = 'b20e65ea-8916-4b86-8d32-6a75b25685d1';
 export const MODAL_CLOSE_SEC = 2.5;
